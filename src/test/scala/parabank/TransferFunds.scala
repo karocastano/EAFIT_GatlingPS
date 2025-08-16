a4d9eb24731defcc3dd2fa5348b6c049ec0d5093
karocastano <karolincastano@gmail.com>
2025-08-16 10:50:28 -0500

    Create TransferFunds.scala

---
+++ src/test/scala/parabank/TransferFunds.scala
@@ -0,0 +1,29 @@
+package parabank
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import parabank.Data._
+
+class TransferFunds extends Simulation{
+
+   // 1 Http Conf
+  val httpConf = http.baseUrl(url)
+    .acceptHeader("application/json")
+    //Verificar de forma general para todas las solicitudes
+    .check(status.is(200))
+
+  // 2 Scenario Definition
+  val scn = scenario("TransferFunds").
+    exec(http("TransferFunds")
+      .post("/transfer")  
+      .queryParam("fromAccountId", fromAccountId)
+      .queryParam("toAccountId", toAccountId)
+      .queryParam("amount", amount)
+      .check(status.is(200))
+       )
+  
+// 3 Load Scenario
+  setUp(
+    scn.inject(rampUsersPerSec(5).to(15).during(30))
+  ).protocols(httpConf);
+}
