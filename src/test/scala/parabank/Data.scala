61a902477740f0ff19857ad05aadf9f97d288a81
karocastano <karolincastano@gmail.com>
2025-08-22 13:13:13 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -3,11 +3,12 @@ package parabank
 object Data{
     val url= "https://parabank.parasoft.com/parabank/services/bank"
     val username = "karocastano"
-    val password = "KaroCastano"
-    val fromAccountId = "14787"
-    val toAccountId = "19116"
+    val password = "karocastano"
+    val fromAccountId = "17784"
+    val toAccountId = "18117"
     val amount = "10"
 
 }
+
 
 
